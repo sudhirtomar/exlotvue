5a2940a2d45d602f159ce703c6c64125c0c95506
Sudhir <sudhirsnv@gmail.com>
2018-04-23 16:44:16 +0530

    make it better

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
 
 	validates :first_name, presence: true
 	validates :last_name, presence: true
-	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, on: :update }
+
 	validates :email, presence: true
 	validates :email, uniqueness: true
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, on: :update }
+	
 end
